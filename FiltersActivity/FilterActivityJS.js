166c61bb88d8d8b1acaa83ed844e5177214b2571
Gowtham <gothsiddu@gmail.com>
2017-01-02 14:19:18 +0530

    Added rainbow filter

--- FiltersActivity/FilterActivityJS.js
+++ FiltersActivity/FilterActivityJS.js
@@ -121,3 +121,94 @@ function blue() {
 	var canvas = document.getElementById("canvas1");
 	image.drawTo(canvas);
 }
+
+function rainbow() {
+	if (null == image || !image.complete()) {
+		alert("No Image");
+		return;
+	}
+	
+	for (var pixel of image.values()) {
+		var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+		var x = pixel.getX();
+		var y = pixel.getY();
+		var width = image.getWidth();
+		var height = image.getHeight();
+		var factor = width / 7;
+		
+		if (y < factor) {
+			if (avg < 128) {
+				pixel.setRed(2 * avg);
+				pixel.setGreen(0);
+				pixel.setBlue(0);
+			} else {
+				pixel.setRed(255);
+				pixel.setGreen(2 * avg - 255);
+				pixel.setBlue(2 * avg - 255);
+			}
+		} else if (y >= factor && y < 2 * factor) {
+			if (avg < 128) {
+				pixel.setRed(2 * avg);
+				pixel.setGreen(0.8 * avg);
+				pixel.setBlue(0);
+			} else {
+				pixel.setRed(255);
+				pixel.setGreen(1.2 * avg - 51);
+				pixel.setBlue(2 * avg - 255);
+			}
+		} else if (y >= 2 * factor && y < 3 * factor) {
+			if (avg < 128) {
+				pixel.setRed(2 * avg);
+				pixel.setGreen(2 * avg);
+				pixel.setBlue(0);
+			} else {
+				pixel.setRed(255);
+				pixel.setGreen(255);
+				pixel.setBlue(2 * avg - 255);
+			}
+		} else if (y >= 3 * factor && y < 4 * factor) {
+			if (avg < 128) {
+				pixel.setRed(0);
+				pixel.setGreen(2 * avg);
+				pixel.setBlue(0);
+			} else {
+				pixel.setRed(2 * avg - 255);
+				pixel.setGreen(255);
+				pixel.setBlue(2 * avg - 255);
+			}
+		}else if (y >= 4 * factor && y < 5 * factor) {
+			if (avg < 128) {
+				pixel.setRed(0);
+				pixel.setGreen(0);
+				pixel.setBlue(2 * avg);
+			} else {
+				pixel.setRed(2 * avg - 255);
+				pixel.setGreen(2 * avg - 255);
+				pixel.setBlue(255);
+			}
+		}else if (y >= 5 * factor && y < 6 * factor) {
+			if (avg < 128) {
+				pixel.setRed(0.8 * avg);
+				pixel.setGreen(0);
+				pixel.setBlue(2 * avg);
+			} else {
+				pixel.setRed(1.2 * avg - 51);
+				pixel.setGreen(2 * avg - 255);
+				pixel.setBlue(255);
+			}
+		}else if (y >= 6 * factor && y < 7 * factor) {
+			if (avg < 128) {
+				pixel.setRed(1.6 * avg);
+				pixel.setGreen(0);
+				pixel.setBlue(1.6 * avg);
+			} else {
+				pixel.setRed(0.4 * avg + 153);
+				pixel.setGreen(2 * avg - 255);
+				pixel.setBlue(0.4 * avg + 153);
+			}
+		}
+	}
+	
+	var canvas = document.getElementById("canvas1");
+	image.drawTo(canvas);
+}
